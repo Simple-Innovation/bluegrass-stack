175616db9d9a14da2f24030a597f39383952bfba
Sebastian Rogers <sebastian.rogers@simpleinnovation.co.uk>
2022-11-05 10:24:56 +0000

    Use Hayseed Dixie image

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -12,8 +12,8 @@ export default function Index() {
             <div className="absolute inset-0">
               <img
                 className="h-full w-full object-cover"
-                src="https://user-images.githubusercontent.com/1500684/157774694-99820c51-8165-4908-a031-34fc371ac0d6.jpg"
-                alt="Sonic Youth On Stage"
+                src="https://user-images.githubusercontent.com/5982044/200115144-4b8c6bb7-1e47-4ed8-8438-b8356de586c3.jpg"
+                alt="Hayseed Dixie On Stage"
               />
               <div className="absolute inset-0 bg-[color:rgba(254,204,27,0.5)] mix-blend-multiply" />
             </div>
