8c584214d13f490e3487c6bb140cc87dd95d416f
Sebastian Rogers <sebastian.rogers@simpleinnovation.co.uk>
2022-12-09 17:47:12 +0000

    Set colour to 'Azure'

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -19,7 +19,7 @@ export default function Index() {
             </div>
             <div className="relative px-4 pt-16 pb-8 sm:px-6 sm:pt-24 sm:pb-14 lg:px-8 lg:pb-20 lg:pt-32">
               <h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
-                <span className="block uppercase text-blue-500 drop-shadow-md">
+                <span className="block uppercase text-azure-500 drop-shadow-md">
                   Bluegrass Stack
                 </span>
               </h1>
@@ -31,7 +31,7 @@ export default function Index() {
                 {user ? (
                   <Link
                     to="/notes"
-                    className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-blue-700 shadow-sm hover:bg-blue-50 sm:px-8"
+                    className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-azure-700 shadow-sm hover:bg-azure-50 sm:px-8"
                   >
                     View Notes for {user.email}
                   </Link>
@@ -39,13 +39,13 @@ export default function Index() {
                   <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
                     <Link
                       to="/join"
-                      className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-blue-700 shadow-sm hover:bg-blue-50 sm:px-8"
+                      className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-azure-700 shadow-sm hover:bg-azure-50 sm:px-8"
                     >
                       Sign up
                     </Link>
                     <Link
                       to="/login"
-                      className="flex items-center justify-center rounded-md bg-blue-500 px-4 py-3 font-medium text-white hover:bg--600"
+                      className="flex items-center justify-center rounded-md bg-azure-500 px-4 py-3 font-medium text-white hover:bg--600"
                     >
                       Log In
                     </Link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        azure: {
+          100: "#CCE8F7",
+          200: "#99D0EF",
+          300: "#66B9E7",
+          400: "#33A1DF",
+          500: "#008AD7",
+          600: "#006EAC",
+          700: "#005381",
+          800: "#003756",
+          900: "#001C2B",
+        },
+      },
+    },
   },
   plugins: [],
 };
